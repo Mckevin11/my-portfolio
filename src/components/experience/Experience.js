c3a18ce9267439957a5b1c88ce620525740d652d
kingmckevin <kingmckevin@gmail.com>
2022-09-29 08:23:09 +0800

    updating portfolios level of mastery on each languages

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -23,28 +23,28 @@ const experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>REACT</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>JavaScript</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>BootStrap</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>
@@ -63,28 +63,28 @@ const experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Express</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MongoDB</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Django</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MySQL</h4>
-                <small className="text-light">Advance</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
           </div>

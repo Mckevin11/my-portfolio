e47200a4d229a2d13401fb466654bb54216fbe71
kingmckevin <kingmckevin@gmail.com>
2022-10-21 08:47:40 +0800

    deployment purposes

--- deployment/src/index.js
+++ deployment/src/index.js
@@ -1,5 +1,12 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
